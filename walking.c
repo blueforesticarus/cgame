8eb4c35abb3c95e0fd064d151fc86d87bf39454f
bficarus <erichspaker@gmail.com>
2016-07-15 23:26:28 -0400

    I added color to the walker demo. Not sure I like it, I think black background + light colors would be better. Also we are going top need a standard terminal font.

--- walking.c
+++ walking.c
@@ -36,17 +36,22 @@ int main(int argc, char *argv[]) {
     curs_set(FALSE);
 	getmaxyx(stdscr, maxy, maxx);
 
-    //start_color();			/* Start color 			*/
+    start_color();			/* Start color 			*/
 	
-    //init_pair(100, 0,5);
-
-
 	pos.x = 20;
 	pos.y = 20;
 	
 	rstring buf = read_file("demoland.ated");
 	ated landscape = parse(buf);
 	
+	for(int i; i<256;i++){
+		init_color(i, (landscape.colors[i].r*1000)/256, (landscape.colors[i].g*1000)/256, (landscape.colors[i].b*1000)/256);
+		init_pair(i, 0,i);
+	}
+
+	//init_pair(landscape.colormap['B'],landscape.colormap['1'],landscape.colormap['B']);//TODO do properly
+	init_pair(landscape.colormap['1'],landscape.colormap['B'],landscape.colormap['B']);//this is hacky
+
 
 	char press;
 	vec2 newpos;
@@ -54,22 +59,26 @@ int main(int argc, char *argv[]) {
 		erase();
 
 		for(int c = 0 ; c<landscape.columns; c++){
-			for(int r = 0 ; r<landscape.rows; r++){	
-				mvaddch(r,c,landscape.states[0].symbols[c+r*landscape.columns]);
+			for(int r = 0 ; r<landscape.rows; r++){
+				attron(COLOR_PAIR( landscape.colormap[landscape.states[0].colormap[c+r*landscape.columns]] ));
+				mvaddch(r,c,landscape.states[0].symbols[c+r*landscape.columns]);	
 			}
 		}
 		
-
-		//attron(COLOR_PAIR(100));
-		if(landscape.states[0].hitmap[newpos.x+(newpos.y-1)*landscape.columns] != 'B' ){
-			if(landscape.states[0].hitmap[newpos.x+(newpos.y-1)*landscape.columns] != 'H' && landscape.states[0].hitmap[newpos.x+(newpos.y-1)*landscape.columns] != 'n' ){
+		//attron(A_BOLD | A_DIM);
+		if(landscape.states[0].hitmap[newpos.x+(pos.y-1)*landscape.columns] != 'B' ){
+			if(landscape.states[0].hitmap[newpos.x+(pos.y-1)*landscape.columns] != 'H' && landscape.states[0].hitmap[pos.x+(pos.y-1)*landscape.columns] != 'n' ){
+				attron(COLOR_PAIR( landscape.colormap[landscape.states[0].colormap[pos.x+(pos.y-1)*landscape.columns]] ));
 				mvaddch(pos.y-1,pos.x,'o');
 			}
-			if(landscape.states[0].hitmap[newpos.x+(newpos.y)*landscape.columns] != 'T' ){
+		}
+		if(landscape.states[0].hitmap[newpos.x+(pos.y)*landscape.columns] != 'B' ){
+			if(landscape.states[0].hitmap[pos.x+(pos.y)*landscape.columns] != 'T' ){
+				attron(COLOR_PAIR( landscape.colormap[landscape.states[0].colormap[pos.x+pos.y*landscape.columns]] ));
 				mvaddch(pos.y,pos.x,'|');
 			}	
 		}
-		//attron(COLOR_PAIR(0))
+		//attroff(A_BOLD);
 
 		refresh();
 		usleep(DELAY);
@@ -81,9 +90,9 @@ int main(int argc, char *argv[]) {
 			newpos.y-=1;
 		}else if(key == KEY_DOWN){
 			newpos.y+=1;
-		}else if(key == KEY_RIGHT || press==KEY_RIGHT){
+		}else if(key == KEY_RIGHT){
 			newpos.x+=1;
-		}else if(key == KEY_LEFT || press==KEY_LEFT){
+		}else if(key == KEY_LEFT){
 			newpos.x-=1;
 		}
 		press=key;
